d53348b35459eac8ef48b0524293c3685ffd8faa
Geetha <geethareddy6482@gmail.com>
2025-11-06 10:51:38 +0530

    added files via upload

---
+++ sdk-local-component-map.js
@@ -0,0 +1,17 @@
+// Statically load all "local" components that aren't yet in the npm package
+
+import AppShell from './src/components/override-sdk/template/AppShell';
+import NavBar from './src/components/override-sdk/infra/NavBar';
+
+/* import end - DO NOT REMOVE */
+
+// localSdkComponentMap is the JSON object where we'll store the components that are
+// found locally. If not found here, we'll look in the Pega-provided component map
+
+const localSdkComponentMap = {
+  AppShell,
+  NavBar
+  /* map end - DO NOT REMOVE */
+};
+
+export default localSdkComponentMap;
